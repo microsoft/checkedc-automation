0a47fc9faaa9e3126f356cc64a0091482c6eb0c6
Mandeep Singh Grang <magrang@microsoft.com>
2020-05-14 13:46:41 -0700

    Fix

--- UNIX/process-benchmarks.sh
+++ UNIX/process-benchmarks.sh
@@ -14,7 +14,7 @@ for TEST_TARGET in $TEST_TARGET_ARCH; do
     exit 1
   fi
 
-  STORAGEACCOUNTNAME=$(StorageAccountName)
+  STORAGEACCOUNTNAME=$StorageAccountName
   export STORAGEACCOUNTNAME
   python extract_benchmark_data.py --logfile $LOGFILE --output-type text --store-to-db
 done
