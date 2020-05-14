2bf8a91aed90ac584f86f8d1136e4d037416c481
Mandeep Singh Grang <magrang@microsoft.com>
2020-05-14 14:11:32 -0700

    Fix

--- UNIX/process-benchmarks.sh
+++ UNIX/process-benchmarks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CURDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-cd $CURDIR
+#CURDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+#cd $CURDIR
 
 BENCHMARK=yes
 source ./config-vars.sh
